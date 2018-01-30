4a8295c2d80488b1cf1188f3aaa94974e9071438
Kyle Roach <kyle.roach@hyuna.bb>
2018-01-30 16:23:09 -0400

    Added enzyme

--- src/containers/__tests__/App.test.tsx
+++ src/containers/__tests__/App.test.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { create } from 'react-test-renderer';
-import App from '../App';
+import React from 'react'
+import { create } from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import App from '../App'
 
 it('it should work', () => {
-  const tree = create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+  const tree = create(<App />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('should just render', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find('h1').text()).toBe('0 new react projects')
+})
